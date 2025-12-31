b590a0417bf75e37e796e429a177d4ccd7a5bafb
Franklin Bello <codetancy@gmail.com>
2025-12-31 17:23:36 -0500

    Sending buffer and adding operator keymap

--- init.lua
+++ init.lua
@@ -1,6 +1,4 @@
-local M = {
-  buffer = -1,
-}
+local M = { buffer = -1 }
 
 local commands = {
   fsi_cmd = "dotnet fsi"
@@ -63,45 +61,101 @@ local add_termination = function(cmd)
   return cmd .. "\n" .. ";;" .. "\n"
 end
 
-local fsi_line = function()
-  local send_current_line = function()
-    -- We can assume that the buffer is validated because
-    -- fsi is called prior
-    local buf_id = M.buffer
-    local chn = vim.bo[buf_id].channel
+local send_lines = function(count, direction)
+  local buf_id = M.buffer
+  local chn = vim.bo[buf_id].channel
 
-    local cmd = vim.api.nvim_get_current_line()
-    vim.api.nvim_chan_send(chn, add_termination(cmd))
+  local current_line = vim.api.nvim_win_get_cursor(0)[1]
+
+  local start = 0
+  local end_ = 0
+  if direction == 'above' then
+    start = current_line - 1
+    end_ = current_line + count
+  elseif direction == 'below' then
+    start = current_line - count
+    end_ = current_line - 1
+  else
+    start = current_line - 1
+    end_ = current_line
   end
 
+  local lines = vim.api.nvim_buf_get_lines(0, start, end_, false)
+  local cmd = table.concat(lines, '\n')
+  vim.api.nvim_chan_send(chn, add_termination(cmd))
+end
+
+local send_selection = function()
+  local buf_id = M.buffer
+  local chn = vim.bo[buf_id].channel
+
+  -- Whenever we select in visual mode, the <> marks are saved
+  -- See :marks for the last selection
+  local open = vim.api.nvim_buf_get_mark(0, "<")
+  local close = vim.api.nvim_buf_get_mark(0, ">")
+  -- Alternatively, we could have yanked selection to a register
+
+  local lines = vim.api.nvim_buf_get_lines(0, open[1] - 1, close[1], true)
+  local cmd = table.concat(lines, '\n')
+  vim.api.nvim_chan_send(chn, add_termination(cmd))
+end
+
+local send_buffer = function()
+  local buf_id = M.buffer
+  local chn = vim.bo[buf_id].channel
+  local lines = vim.api.nvim_buf_get_lines(0, 0, -1, true)
+  local cmd = table.concat(lines, '\n')
+  vim.api.nvim_chan_send(chn, add_termination(cmd))
+end
+
+local send_motion = function()
+  local buf_id = M.buffer
+  local chn = vim.bo[buf_id].channel
+
+  local open = vim.api.nvim_buf_get_mark(0, "[")
+  local close = vim.api.nvim_buf_get_mark(0, "]")
+
+  local lines = vim.api.nvim_buf_get_lines(0, open[1] - 1, close[1], false)
+  local cmd = table.concat(lines, '\n')
+  vim.api.nvim_chan_send(chn, add_termination(cmd))
+end
+
+local fsi_line = function(opts)
   fsi()
-  vim.defer_fn(send_current_line, 1000)
+  vim.defer_fn(function() send_lines(opts.count, opts.args) end, 1000)
 end
 
 local fsi_select = function()
-  local send_selection = function()
-    -- We can assume that the buffer is validated because
-    -- fsi is called prior
-    local buf_id = M.buffer
-    local chn = vim.bo[buf_id].channel
-
-    -- Whenever we select in visual mode, the <> marks are saved
-    -- See :marks for the last selection
-    local open = vim.api.nvim_buf_get_mark(0, "<")
-    local close = vim.api.nvim_buf_get_mark(0, ">")
-    -- Alternatively, we could have yanked selection to a register
-
-    local lines = vim.api.nvim_buf_get_lines(0, open[1] - 1, close[1], true)
-    local cmd = table.concat(lines, '\n')
-    vim.api.nvim_chan_send(chn, add_termination(cmd))
-  end
-
   fsi()
   vim.defer_fn(send_selection, 1000)
 end
 
-vim.api.nvim_create_user_command("Fsi", fsi, {})
-vim.api.nvim_create_user_command("FsiLine", fsi_line, {})
-vim.api.nvim_create_user_command("FsiSelect", fsi_select, {})
--- TODO: Create cmd for sending entire buffer
--- TODO: Update line cmd for accepting number of lines
+local fsi_buffer = function()
+  fsi()
+  vim.defer_fn(send_buffer, 1000)
+end
+
+local fsi_motion = function(_)
+  fsi()
+  vim.defer_fn(send_motion, 1000)
+end
+_G.fsi_motion = fsi_motion
+
+vim.api.nvim_create_user_command("Fsi", fsi,
+  { desc = 'Opens fsharp interactive' })
+vim.api.nvim_create_user_command("FsiLine", fsi_line,
+  { count = 0, nargs = 1, desc = 'Sends n lines to fsharp interactive' })
+vim.api.nvim_create_user_command("FsiSelect", fsi_select,
+  { desc = 'Sends visual mode selection to fsharp interactive' })
+vim.api.nvim_create_user_command("FsiBuffer", fsi_buffer,
+  { desc = 'Sends current buffer contents to fsharp interactive' })
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "fsharp", "fs" },
+  callback = function(ev)
+    vim.keymap.set('n', '<leader>i', function()
+      vim.o.operatorfunc = 'v:lua.fsi_motion'
+      return 'g@'
+    end, { expr = true, buffer = ev.buf })
+  end
+})
